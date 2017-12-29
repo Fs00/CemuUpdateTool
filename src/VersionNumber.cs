76b7c5d7de287d3ed2cd5db5ab52c53969515cdd
Fs00 <francescosaltori@gmail.com>
2017-12-29 11:37:27 +0100

    Implemented more VersionNumber methods, added more tests for VersionNumber

--- src/VersionNumber.cs
+++ src/VersionNumber.cs
@@ -76,7 +76,7 @@ namespace CemuUpdateTool
             fields.Add(0);      // Depth must never be 0
         }
 
-        // Constructor that takes in input a string like "1.5.2" (separator is '.')
+        // Constructor that takes in input a string like "1.5.2" (only allowed separator is '.')
         public VersionNumber(string version)
         {
             string[] splittedVersionNr = version.Split('.');
@@ -91,18 +91,60 @@ namespace CemuUpdateTool
             if (depth < 1 || depth > 4)
                 throw new ArgumentException("Depth value is not valid.");
 
-            fields = new List<int>(depth);
-            fields[0] = versionInfo.FileMajorPart;
+            fields = new List<int>();
+            fields.Add(versionInfo.FileMajorPart);
             if (depth > 1)
             {
-                fields[1] = versionInfo.FileMinorPart;
+                fields.Add(versionInfo.FileMinorPart);
                 if (depth > 2)
                 {
-                    fields[2] = versionInfo.FileBuildPart;
+                    fields.Add(versionInfo.FileBuildPart);
                     if (depth > 3)
-                        fields[3] = versionInfo.FilePrivatePart;
+                        fields.Add(versionInfo.FilePrivatePart);
                 }
             }
+        }
+
+        /*
+         *  ToString() methods. Custom ToString(char) method permits to supply a custom separator between fields (even '' if you want).
+         *  Overridden ToString() uses custom ToString() with default separator (which is '.')
+         */
+        public string ToString(char separator)
+        {
+            string output = "";
+            for (int i = 0; i < Depth; i++)
+            {
+                output += fields[i];
+                if (i < Depth-1)
+                    output += separator.ToString();
+            }
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return ToString('.');
+        }
+
+        /*
+         *  Methods for increasing/decreasing depth. You can also increase depth by more than 1 at a time.
+         */
+        public void IncreaseDepth(int extraDepth)
+        {
+            for (int i = 0; i < extraDepth; i++)
+                fields.Add(0);
+        }
+
+        public void IncreaseDepth()
+        {
+            IncreaseDepth(1);
+        }
+
+        public void DecreaseDepth()
+        {
+            if (Depth <= 1)
+                throw new InvalidOperationException("Depth can't be less than 1.");
+            fields.RemoveAt(Depth-1);
         }
     }
 }
--- tests/VersionNumberTests.cs
+++ tests/VersionNumberTests.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using CemuUpdateTool;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CemuUpdateTool.tests
+namespace CemuUpdateTool.Tests
 {
     [TestClass]
     public class VersionNumberTests
@@ -24,7 +24,60 @@ namespace CemuUpdateTool.tests
         {
             string vNumber = "1.5";
             VersionNumber version = new VersionNumber(vNumber);
-            Console.WriteLine(version.Build);   // should cause InvalidOperationException
+            Console.WriteLine(version.Build);   // should throw InvalidOperationException
+        }
+
+        [TestMethod]
+        public void ValidFileVersionInfoConstructor_StandardDepth()
+        {
+            FileVersionInfo testFile = null;
+            try
+            {
+                testFile = FileVersionInfo.GetVersionInfo("./versionTest.exe");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Assert.Inconclusive("Test file not found.");
+            }
+
+            VersionNumber version = new VersionNumber(testFile);
+            Assert.AreEqual(version.Depth, 4);
+            Assert.AreEqual(version.Major, 1);
+            Assert.AreEqual(version.Minor, 11);
+            Assert.AreEqual(version.Build, 2);
+            Assert.AreEqual(version.Private, 0);
+        }
+
+        [TestMethod]
+        public void ValidFileVersionInfoConstructor_CustomDepth()
+        {
+            FileVersionInfo testFile = null;
+            try
+            {
+                testFile = FileVersionInfo.GetVersionInfo("./versionTest.exe");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Assert.Inconclusive("Test file not found.");
+            }
+
+            VersionNumber version = new VersionNumber(testFile, 2);
+            Assert.AreEqual(version.Depth, 2);
+            Assert.AreEqual(version.Major, 1);
+            Assert.AreEqual(version.Minor, 11);
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string vNumber1 = "1.5.2";
+            string vNumber2 = "3";
+
+            VersionNumber version1 = new VersionNumber(vNumber1);
+            VersionNumber version2 = new VersionNumber(vNumber2);
+
+            Assert.AreEqual(version1.ToString(), vNumber1, "Failed version1.ToString()");
+            Assert.AreEqual(version2.ToString(), vNumber2, "Failed version2.ToString()");
         }
     }
 }
