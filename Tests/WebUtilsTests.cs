9e74297382077f45ee543857731647dcebcd3afb
Fs00 <francescosaltori@gmail.com>
2019-07-29 19:28:54 +0200

    Update WebUtilsTests after latest changes

--- Tests/WebUtilsTests.cs
+++ Tests/WebUtilsTests.cs
@@ -1,60 +1,60 @@
 ﻿using System;
+using System.Threading;
 using CemuUpdateTool.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CemuUpdateTool.Tests
 {
-    [TestClass()]
+    [TestClass]
     public class WebUtilsTests
     {
         // TO BE UPDATED WHEN THERE'S A NEW CEMU VERSION
         private readonly VersionNumber latestKnownCemuVersion = new VersionNumber(1, 15, 10);
 
-        private readonly string cemuUrlPrefix = "http://cemu.info/releases/cemu_";
-        private readonly string cemuUrlSuffix = ".zip";
+        private const string CEMU_URL_PREFIX = "http://cemu.info/releases/cemu_";
+        private const string CEMU_URL_SUFFIX = ".zip";
 
-        private readonly RemoteVersionChecker versionChecker;
-
-        public WebUtilsTests()
+        private RemoteVersionChecker CreateDefaultVersionCheckerForCemu()
         {
-            versionChecker = new RemoteVersionChecker(cemuUrlPrefix, cemuUrlSuffix, 3);
+            return new RemoteVersionChecker(CEMU_URL_PREFIX, CEMU_URL_SUFFIX, 3);
         }
 
-        [TestMethod()]
+        [TestMethod]
         [ExpectedException(typeof(OperationCanceledException))]
         public void RemoteVersionCheckTest_CancelledBeforeStarting()
         {
-            versionChecker.GetLatestRemoteVersionInBranch(
-                VersionNumber.Empty,
-                null,
-                new System.Threading.CancellationToken(true)
-            );
+            var versionChecker = new RemoteVersionChecker(CEMU_URL_PREFIX, CEMU_URL_SUFFIX, 3, new CancellationToken(true));
+            versionChecker.GetLatestRemoteVersionInBranch(VersionNumber.Empty);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void RemoteVersionCheckTest_NoStartingVersion()
         {
+            var versionChecker = CreateDefaultVersionCheckerForCemu();
             VersionNumber result = versionChecker.GetLatestRemoteVersionInBranch(VersionNumber.Empty);
             Assert.AreEqual(latestKnownCemuVersion, result);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void RemoteVersionCheckTest_CorrectStartingVersion()
         {
+            var versionChecker = CreateDefaultVersionCheckerForCemu();
             VersionNumber result = versionChecker.GetLatestRemoteVersionInBranch(VersionNumber.Empty, new VersionNumber(1, 10, 0));
             Assert.AreEqual(latestKnownCemuVersion, result);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void RemoteVersionCheckTest_WrongStartingVersion()
         {
+            var versionChecker = CreateDefaultVersionCheckerForCemu();
             VersionNumber result = versionChecker.GetLatestRemoteVersionInBranch(VersionNumber.Empty, new VersionNumber(65, 10, 0));
             Assert.AreEqual(latestKnownCemuVersion, result);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void RemoteVersionCheckTest_UselessExistingStartingVersion()
         {
+            var versionChecker = CreateDefaultVersionCheckerForCemu();
             VersionNumber latestKnownCemuVersion = new VersionNumber(1, 4, 2);     // latest version of branch 1.4.x
 
             VersionNumber result = versionChecker.GetLatestRemoteVersionInBranch(new VersionNumber(1,4), new VersionNumber(1, 7, 5));
